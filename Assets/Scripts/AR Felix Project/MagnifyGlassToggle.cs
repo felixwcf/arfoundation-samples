5fa4f5d86cd65e4bd78b62f4e8d7b786317cf11a
Chan Fui <felixwcf85@hotmail.com>
2020-08-18 14:51:28 +0800

    magnify glass toggle button on/off.

---
+++ Assets/Scripts/AR Felix Project/MagnifyGlassToggle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagnifyGlassToggle : MonoBehaviour
+{
+    [SerializeField] MagnifyGlass magnifyGlass;
+    [SerializeField] Text buttonText;
+
+    GameObject magnifyCameraObject;
+    bool hasMagnifyCam;
+    bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonText.text = "Magnify Glass: " + (isOn ? "ON" : "OFF");
+    }
+
+    private void Update()
+    {
+        if(!hasMagnifyCam)
+        {
+            if (GameObject.Find("MagnifyCamera"))
+            {
+                hasMagnifyCam = true;
+                magnifyCameraObject = GameObject.Find("MagnifyCamera");
+                magnifyCameraObject.SetActive(false);
+            }
+        }
+    }
+
+    public void toggleButtonDidTapped()
+    {
+        isOn = !isOn;
+        magnifyCameraObject.SetActive(isOn);
+        buttonText.text = "Magnify Glass: " + (isOn?"ON" : "OFF");
+    }
+}
